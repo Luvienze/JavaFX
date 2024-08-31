689a8a38614f56f2d90b0b30fd77dddd86ff80ed
Luvienze <94405625+Luvienze@users.noreply.github.com>
2024-08-31 13:41:22 +0300

    files are uploaded

---
+++ PreConstructedController/src/main/java/com/luvienze/preconstructedcontroller/App.java
@@ -0,0 +1,22 @@
+package com.luvienze.preconstructedcontroller;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class App extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("main.fxml"));
+        Controller controller = new Controller("Hello World!");
+        fxmlLoader.setController(controller);
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
---
+++ PreConstructedController/src/main/java/com/luvienze/preconstructedcontroller/Controller.java
@@ -0,0 +1,25 @@
+package com.luvienze.preconstructedcontroller;
+
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+
+    public Label message_lbl;
+    public Button message_btn;
+
+    private final String message;
+
+    public Controller(String message){
+        this.message = message;
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        message_btn.setOnAction(event -> message_lbl.setText("Message: " + message));
+    }
+}
